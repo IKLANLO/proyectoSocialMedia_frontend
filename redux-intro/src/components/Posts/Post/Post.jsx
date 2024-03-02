c8f8eec1ba2308404409b17599bb43a998f8d2b2
IKLANLO <ilandaberea@yahoo.es>
2024-03-02 21:10:37 +0100

    añadidos likes de Posts a Home

--- redux-intro/src/components/Posts/Post/Post.jsx
+++ redux-intro/src/components/Posts/Post/Post.jsx
@@ -9,14 +9,15 @@ const Post = () => {
         <div key={post._id}>
           <h2>{post.name}</h2>
           <p>{post.post}</p>
-          {post.comments.length > 0 && <h3>Comentarios:</h3>}
+          { post.likes.length === 1 ? <h3>{post.likes.length} like</h3> : <h3>{post.likes.length} likes</h3>}
+          {post.comments.length > 0 && <h3>{post.comments.length} comentarios: </h3>}
           {post.comments.map((comment) => {
             return (
               <div key={comment._id}>
                 <p>{comment.comment}</p>
               </div>
             )
-      })}
+          })}
         </div>
       ))}
     </>
