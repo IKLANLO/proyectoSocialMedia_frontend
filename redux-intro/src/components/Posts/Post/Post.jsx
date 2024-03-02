617270d51a44458ac3b34957902ca8a7fc93fa91
IKLANLO <ilandaberea@yahoo.es>
2024-03-02 21:46:44 +0100

    añadidas Carts a Post

--- redux-intro/src/components/Posts/Post/Post.jsx
+++ redux-intro/src/components/Posts/Post/Post.jsx
@@ -1,28 +1,31 @@
 import { useSelector } from 'react-redux'
 import { LikeOutlined } from '@ant-design/icons';
-import { Statistic } from 'antd';
+import { Statistic, Card } from 'antd';
 
 const Post = () => {
   const { posts } = useSelector((state) => state.posts)
   
   return (
-    <>
-      {posts.map((post)=> (
+      <>
+        {posts.map((post)=> (
         <div key={post._id}>
-          <h2>{post.name}</h2>
-          <p>{post.post}</p>
-          <Statistic value={post.likes.length} prefix={<LikeOutlined style={{color:'white'}} />}/>
-          {post.comments.length > 0 && <h3>{post.comments.length} comentarios: </h3>}
-          {post.comments.map((comment) => {
-            return (
-              <div key={comment._id}>
-                <p>{comment.comment}</p>
-              </div>
-            )
-          })}
+          <Card title={post.name}>
+            <p>{post.post}</p>
+            <Statistic value={post.likes.length} prefix={<LikeOutlined />}/>
+            {post.comments.length > 0 && <h3>{post.comments.length} comentarios </h3>}
+            {post.comments.map((comment) => {
+              return (
+                <div key={comment._id}>
+                  <Card type="inner">
+                    {comment.comment}
+                  </Card>
+                </div>
+              )
+            })}
+          </Card>
         </div>
-      ))}
-    </>
+        ))}
+      </>
     )
   }
   
