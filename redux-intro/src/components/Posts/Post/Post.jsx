d2d95803611e0bfdab481d85a53e245656b2352f
IKLANLO <ilandaberea@yahoo.es>
2024-03-02 22:11:36 +0100

    añadido estilo a Post

--- redux-intro/src/components/Posts/Post/Post.jsx
+++ redux-intro/src/components/Posts/Post/Post.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
 import { LikeOutlined } from '@ant-design/icons';
 import { Statistic, Card } from 'antd';
+import './Post.styles.scss'
 
 const Post = () => {
   const { posts } = useSelector((state) => state.posts)
@@ -9,13 +10,17 @@ const Post = () => {
       <>
         {posts.map((post)=> (
         <div key={post._id}>
-          <Card title={post.name}>
-            <p>{post.post}</p>
-            <Statistic value={post.likes.length} prefix={<LikeOutlined />}/>
-            {post.comments.length > 0 && <h3>{post.comments.length} comentarios </h3>}
+          <Card title={post.name} className='card-container'>
+            <div className='card-container__leftalign'>
+              <p>{post.post}</p>
+            </div>
+            <Statistic className='card-container__rightalign'
+               value={post.likes.length} prefix={<LikeOutlined />}/>
+            {post.comments.length > 0 && 
+              <h3 className='card-container__leftalign'>{post.comments.length} comentarios </h3>}
             {post.comments.map((comment) => {
               return (
-                <div key={comment._id}>
+                <div key={comment._id} className='card-container__leftalign'>
                   <Card type="inner">
                     {comment.comment}
                   </Card>
